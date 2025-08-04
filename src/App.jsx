dcae90eaea359078953f28a84c780964ac3dccd7
MMakram <kikoftw151@gmai.com>
2025-08-04 04:22:22 +0300

    changejsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import About from './Components/About/About';
 import Home from './Components/Home/Home';
@@ -8,8 +5,6 @@ import Layout from './Components/Layout/Layout';
 import Contact from './Components/Contact/Contact';
 import Portfolio from './Components/Portfolio/Portfolio';
 import NotFound from './Components/NotFound/NotFound';
-import Footer from './Components/Footer/footer';
-import Navbar from './Components/Navbar/Navbar';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
